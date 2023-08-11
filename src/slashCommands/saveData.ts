f88193cb8bd5235e1cabe423f68c670b8a2fcb7e
Étienne S <sauvage-etienne@hotmail.com>
2023-08-11 14:06:59 +0200

    Reimplement permissions

--- src/slashCommands/saveData.ts
+++ src/slashCommands/saveData.ts
@@ -1,10 +1,13 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, PermissionsBitField } from 'discord.js';
 import { saveModifiedData } from '../config/database';
 import { SlashCommand } from './../types';
 
 export const command: SlashCommand = {
   name: 'savedata',
-  data: new SlashCommandBuilder().setName('savedata').setDescription('Force la sauvegarde des données.'),
+  data: new SlashCommandBuilder()
+    .setName('savedata')
+    .setDescription('Force la sauvegarde des données.')
+    .setDefaultMemberPermissions(PermissionsBitField.Flags.ModerateMembers),
   execute: async (interaction) => {
     saveModifiedData();
     await interaction.reply({
